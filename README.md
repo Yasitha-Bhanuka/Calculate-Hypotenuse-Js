24ec2e66af623cbe9c10cd381f855b44b588bc98
yasitha <it22563750@my.sliit.lk>
2023-07-08 10:28:52 +0530

    get input using html input text boxes.
    include the onclick function for it, and run the Js funtion assign the values for the variables

--- README.md
+++ README.md
@@ -16,3 +16,7 @@ I used to get the input from the user prompt window.
 Check weather conditions is true user can see the hypotneuse in the display.
 
 It is doing by document.getElementById("ID_name").innerHTML = "Hypotenuse is" + c; 
+
+
+in the socond way. we can get the input with the html input boxes and dispay the outputs
+in Js file line number 33 and in the html line number 12 to 18.
--- index.js
+++ index.js
@@ -6,24 +6,37 @@ let a;
 let b;
 let c;
 
-
-do{
+// get input by prompt. line number 10 to 29
+/*do{
     window.alert("Enter the number for the inputs");
     
-    // get the input from the user using prompt
-    a = window.prompt("Enter side A ");
+//     // get the input from the user using prompt
+     a = window.prompt("Enter side A ");
     
-} while (isNaN(a));
+ } while (isNaN(a));
 
-if (a != null){
+ if (a != null){
 
     b = window.prompt("Enter side B ");    
 
-}
+ }
 
-// claculate the hypoteneuse
+// // claculate the hypoteneuse
 c = Math.pow(a, 2) + Math.pow(b, 2);
 c = Math.sqrt(c);
 
-// bring the out put into the index web page.
-document.getElementById("Answer").innerHTML = "Hypotenuse of the right angle rectangle : " + c; 
+// // bring the out put into the index web page.
+document.getElementById("Answer").innerHTML = "Hypotenuse of the right angle rectangle : " + c; */
+
+// lets we look the get inputs from the html input boxes.line number 33 to 42
+
+document.getElementById("submitButton").onclick = function(){
+    a = document.getElementById("sideA").value;
+    a = Number(a);
+    
+    b = document.getElementById("sideB").value;
+    b = Number(b);
+    
+    c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+    document.getElementById("cLabel").innerHTML = "value of side C : "+ c;
+}
